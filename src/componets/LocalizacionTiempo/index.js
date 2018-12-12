cbeecf5c0c33168cd01ad680580e78bcce30b13f
Juan Alcocer <juanalcocer95@gmail.com>
2018-12-12 17:59:17 +0100

    añadido loader

--- src/componets/LocalizacionTiempo/index.js
+++ src/componets/LocalizacionTiempo/index.js
@@ -1,27 +1,11 @@
 import React, { Component } from 'react';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import Localizacion from './Localizacion';
 import DatosTiempo from './DatosTiempo';
 import transformarTiempo from './../../services/transformarTiempo';
 import { api_tiempo } from './../../constants/api_url';
+
 import './style.css';
-
-import {
-    SOL,
-    NIEBLA,
-    NUBE,
-    NUBLADO,
-    LLUVIA,
-    NIEVE,
-    VIENTO,
-} from './../../constants/weathers';
-
-const data = {
-    temperatura: 5,
-    estadoTiempo: SOL,
-    humedad: 10,
-    viento: "10 m/s",
-}
-
 
 class LocalizacionTiempo extends Component {
 
@@ -29,8 +13,15 @@ class LocalizacionTiempo extends Component {
         super();
         this.state = {
             city: 'Puerto de Sagunto',
-            data: data,
+            data: null,
         };
+    }
+
+    componentDidMount() {
+        this.updateClick();
+    }
+    componentDidUpdate() {
+
     }
 
     updateClick = () => {
@@ -48,11 +39,13 @@ class LocalizacionTiempo extends Component {
     }
 
     render() {
+        
         return (
             <div className="ContLocalizacionTiempo">
                 <Localizacion ciudad={this.state.city}></Localizacion>
-                <DatosTiempo data={this.state.data} ></DatosTiempo>
-                <button onClick={this.updateClick}>Actualizar</button>
+                {this.state.data ? 
+                    <DatosTiempo data={this.state.data} ></DatosTiempo> 
+                        : <CircularProgress /> }
             </div>
         );
     }
--- src/constants/api_url.js
+++ src/constants/api_url.js
@@ -1,5 +1,5 @@
 
-const api_key = "47665e9fee7c7fd9c39401b501e322b1"; //f99bbd9e4959b513e9bd0d7f7356b38d
+const api_key = "47665e9fee7c7fd9c39401b501e322b1";
 const url_base = "https://api.openweathermap.org/data/2.5/weather";
 const location = "Sagunto,es";
 
